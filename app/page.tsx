0912e4a91fb47a14f3fb9c90ebdd766e339188dc
Sohaib42 <sohaib.zeghouani@gmail.com>
2023-11-08 15:52:32 +0100

    added font navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,36 +1,34 @@
-import  React from "react";
+import React from "react";
 import Image from "next/image";
 import Carousel from "./components/Carousel";
 
 export default function Home() {
   return (
     <div className="bg-zinc-300 mb-0 flex-col flex mt-px max-sm:h-full">
-      <div className="justify-center bg-gray-600 bg-opacity-90 self-stretch w-full flex-col flex items-center shadow-[rgba(17,17,26,0.1)_0px_1px_0px,rgba(17,17,26,0.1)_0px_8px_24px,rgba(17,17,26,0.1)_0px_16px_48px] mt-px px-20 py-7 rounded-sm max-md:max-w-full max-md:px-5">
-        <div className="relative shrink-0 box-border flex flex-col items-stretch w-full mt-5">
-          <div className="gap-5 flex max-md:flex-col max-md:items-stretch max-md:gap-0">
-            <div className="flex flex-col items-stretch w-6/12 max-md:w-full max-md:ml-0">
+      <div className="justify-center bg-cover bg-center bg-no-repeat self-stretch w-full flex-col flex items-center shadow-[rgba(17,17,26,0.1)_0px_1px_0px,rgba(17,17,26,0.1)_0px_8px_24px,rgba(17,17,26,0.1)_0px_16px_48px] mt-px px-20 py-7 rounded-sm max-md:max-w-full max-md:px-5 bg-gray-600 bg-opacity-50 backdrop-filter backdrop-blur-lg"
+        style={{ backgroundImage: "url('/navfont.jpg')" }}>
+        <div className="relative shrink-0 box-border flex flex-col items-stretch mt-5">
+          <div className="flex justify-center items-center bg-opacity-20 backdrop-blur-sm backdrop-blur-md rounded-lg">
+            <div className="flex flex-col items-center p-4 rounded-lg">
               <Image
-              alt="Navbar"
-              width={120}
-              height={100}
+                alt="Navbar"
+                width={120}
+                height={100}
                 loading="lazy"
                 src="/Pieces-Auto-LOGO.png"
-                className="aspect-[1.73] object-contain object-center w-[157px] overflow-hidden max-w-full mr-auto self-start max-md:mx-auto"
+                className="object-contain w-[157px] overflow-hidden"
               />
-            </div>
-            <div className="flex flex-col items-stretch w-6/12 ml-5 max-md:w-full max-md:ml-0">
-              <div className="relative shrink-0 box-border h-auto w-auto self-center whitespace-nowrap text-xs text-white font-light my-auto max-sm:w-auto max-sm:self-stretch max-sm:text-center">
-              <h1 className="text-base sm:text-3xs md:text-base lg:text-3xl font-semibold text-white leading-normal text-center mx-auto my-2">
-                    Votre fournisseur de confiance en pièces automobiles
-                    détachées
-                </h1>
-              </div>
+              <h1 className="text-base md:text-xl lg:text-3xl font-semibold text-white leading-normal text-center my-2">
+                Votre fournisseur de confiance en pièces automobiles détachées
+              </h1>
             </div>
           </div>
+
         </div>
       </div>
+
       <div className="bg-zinc-300 self-stretch z-[1] flex min-h-full w-full flex-col mb-5 max-md:max-w-full max-md:mb-10">
-      <Carousel images={[
+        <Carousel images={[
           {
             url: 'https://source.unsplash.com/random/800x600',
             alt: 'Random Image 1',
@@ -102,7 +100,7 @@ export default function Home() {
         >
           <section className="flex flex-col relative shrink-0 box-border min-h-[74px] bg-zinc-300 w-full self-stretch grow max-w-[1200px] items-stretch mt-5 mx-auto p-5 p-2 rounded border-none max-sm:min-h-[306px] max-sm:bg-zinc-300 max-sm:rounded-[71px]">
             <div className="relative shrink-0 box-border h-auto mt-5 mx-auto">
-            <h2 className="section-title">Livraison</h2>
+              <h2 className="section-title">Livraison</h2>
             </div>{" "}
             <div className="relative shrink-0 box-border h-auto text-center mt-5 mx-auto max-sm:h-auto max-sm:grow-0">
               <p>
@@ -120,37 +118,37 @@ export default function Home() {
         </div>
       </div>
       <footer className="bg-[rgb(75,85,99)] text-white">
-      <div className="max-w-6xl mx-auto px-4 py-8">
-        <div className="flex flex-wrap justify-between">
-          <div className="w-full sm:w-auto mb-4 sm:mb-0">
-            <h5 className="text-lg font-bold mb-2">Pièces auto (Maxfil)</h5>
-            <p className="text-sm">
-              Votre fournisseur de confiance en pièces automobiles détachées.
+        <div className="max-w-6xl mx-auto px-4 py-8">
+          <div className="flex flex-wrap justify-between">
+            <div className="w-full sm:w-auto mb-4 sm:mb-0">
+              <h5 className="text-lg font-bold mb-2">Pièces auto (Maxfil)</h5>
+              <p className="text-sm">
+                Votre fournisseur de confiance en pièces automobiles détachées.
+              </p>
+            </div>
+            <div className="w-full sm:w-auto">
+              <h5 className="text-lg font-bold mb-2">Navigation</h5>
+              <ul className="text-sm">
+                <li className="mb-1">
+                  <a href="/about" className="hover:underline">À propos de nous</a>
+                </li>
+                <li className="mb-1">
+                  <a href="/products" className="hover:underline">Nos produits</a>
+                </li>
+                <li className="mb-1">
+                  <a href="/contact" className="hover:underline">Contact</a>
+                </li>
+              </ul>
+            </div>
+            {/* Ajouter d'autres sections si nécessaire */}
+          </div>
+          <div className="border-t border-gray-700 mt-8 pt-4">
+            <p className="text-sm text-center">
+              © {new Date().getFullYear()} Pièces auto (Maxfil). Tous droits réservés.
             </p>
           </div>
-          <div className="w-full sm:w-auto">
-            <h5 className="text-lg font-bold mb-2">Navigation</h5>
-            <ul className="text-sm">
-              <li className="mb-1">
-                <a href="/about" className="hover:underline">À propos de nous</a>
-              </li>
-              <li className="mb-1">
-                <a href="/products" className="hover:underline">Nos produits</a>
-              </li>
-              <li className="mb-1">
-                <a href="/contact" className="hover:underline">Contact</a>
-              </li>
-            </ul>
-          </div>
-          {/* Ajouter d'autres sections si nécessaire */}
         </div>
-        <div className="border-t border-gray-700 mt-8 pt-4">
-          <p className="text-sm text-center">
-            © {new Date().getFullYear()} Pièces auto (Maxfil). Tous droits réservés.
-          </p>
-        </div>
-      </div>
-    </footer>
+      </footer>
     </div>
   );
 }
