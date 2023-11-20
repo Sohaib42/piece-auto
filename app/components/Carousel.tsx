463cef5fa24d8fcd1606f833ee1b847d45bbd663
Shinta <sohaib.zeghouani@gmail.com>
2023-11-20 13:20:42 +0100

    fixed height

--- app/components/Carousel.tsx
+++ app/components/Carousel.tsx
@@ -1,10 +1,7 @@
-
 'use client'
-// N'oubliez pas d'importer Image depuis 'next/image'
 import Image from 'next/image';
 import React, { useState, useEffect } from 'react';
 
-// Définissez une interface pour les propriétés de vos images
 interface CarouselImage {
   url: string;
   alt: string;
@@ -27,45 +24,40 @@ const Carousel: React.FC<CarouselProps> = ({ images }) => {
   };
 
   useEffect(() => {
-    const timer = setTimeout(() => {
-      goToNextSlide();
-    }, 5000);
+    const timer = setTimeout(goToNextSlide, 5000);
     return () => clearTimeout(timer);
-  }, [goToNextSlide, currentImageIndex, images.length]);
+  }, [currentImageIndex, images.length]);
 
   return (
     <div className="relative carousel-wrap">
-      {/* Bouton Précédent */}
       <button
-        className="absolute top-1/2 left-2 z-10 p-2 text-white bg-black bg-opacity-50 rounded"
+        className="absolute top-1/2 left-2 z-10 p-2 text-white bg-black bg-opacity-50 rounded sm:left-1"
         onClick={goToPreviousSlide}
         aria-label="Previous image"
       >
         &#10094;
       </button>
 
-      {/* Image du Carrousel */}
       {images.length > 0 && (
-        <div className="carousel-content">
-          <div style={{ width: '50vw', height: '80vh' }}>
-            <Image
-              src={images[currentImageIndex].url}
-              alt={images[currentImageIndex].alt}
-              layout="fill" // Utilisez "fill" pour que l'image remplisse le conteneur
-              objectFit="cover"
-              priority={true}
-            />
-          </div>
 
-          <p className="text-center text-white bg-black bg-opacity-50">
+<div className="carousel-content">
+  <div className="w-64 h-48 md:w-96 md:h-72 lg:w-128 lg:h-96">
+    <Image
+      src={images[currentImageIndex].url}
+      alt={images[currentImageIndex].alt}
+      layout="fill"
+      objectFit="cover"
+      priority={true}
+    />
+  </div>
+          <p className="text-center text-white bg-black bg-opacity-50 text-base sm:text-lg">
             {images[currentImageIndex].legend}
           </p>
         </div>
       )}
 
-      {/* Bouton Suivant */}
       <button
-        className="absolute top-1/2 right-2 z-10 p-2 text-white bg-black bg-opacity-50 rounded"
+        className="absolute top-1/2 right-2 z-10 p-2 text-white bg-black bg-opacity-50 rounded sm:right-1"
         onClick={goToNextSlide}
         aria-label="Next image"
       >
