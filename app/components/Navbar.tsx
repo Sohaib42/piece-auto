8ef360412cc1cda791ccbe4de4fcf25742828c39
Shinta <sohaib.zeghouani@gmail.com>
2023-12-05 15:00:19 +0100

    added missing photos, and mobile navbar version

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -2,8 +2,21 @@ import React from 'react';
 
 export const Navbar = (): JSX.Element => {
   return (
-    <div className="w-full overflow-hidden"> 
-      <img src='banner.svg' alt="Navbar" className="w-full h-auto" />
+    <div className="w-full overflow-hidden">
+      {/* SVG pour les grands écrans */}
+      <img 
+        src='banner.svg'
+        alt="Navbar" 
+        className="hidden sm:block w-full h-auto" 
+      />
+
+      {/* SVG pour les mobiles */}
+      <img 
+        src='mobile-banner.svg' 
+        alt="Mobile Navbar" 
+        className="block sm:hidden w-full h-auto" 
+      />
     </div>
   );
 };
+
